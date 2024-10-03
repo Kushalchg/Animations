d0b512d48c960dc7b24b300442953229d7de2aa1
kushalchg <chapagainkushal74@gmail.com>
2024-10-03 18:49:58 +0545

    tested nepali picker

--- src/screens/HocTestScreen.tsx
+++ src/screens/HocTestScreen.tsx
@@ -1,5 +1,6 @@
-import {View, Text, TextInput} from 'react-native';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
 import React, {useState} from 'react';
+import {CalendarPicker} from 'react-native-nepali-picker';
 
 const InputComponent = ({children}: any) => {
   const [value, setValue] = useState<string>('0');
@@ -37,6 +38,12 @@ const Fahrenheit = ({value}: {value: string}) => {
 };
 
 const HocTestScreen = () => {
+  const [visible, setVisible] = useState<boolean>(false);
+  const [date, setDate] = useState<string>();
+
+  const onPicked = (date: string) => {
+    setDate(date);
+  };
   return (
     <View>
       <Text className="text-black font-spacegrotesk-medium text-xl">
@@ -50,64 +57,24 @@ const HocTestScreen = () => {
           </>
         )}
       </InputComponent>
+      <View>
+        <CalendarPicker
+          visible={visible}
+          onClose={() => setVisible(false)}
+          onDateSelect={onPicked}
+          language="np"
+          theme="dark"
+        />
+      </View>
+      <TouchableOpacity
+        className="bg-purple-500 px-6 py-2 "
+        onPress={() => setVisible(true)}>
+        <Text className="text-black">Pick</Text>
+      </TouchableOpacity>
+
+      <Text className="text-black">{date}</Text>
     </View>
   );
 };
 
 export default HocTestScreen;
-
-// import {View, Text, TextInput} from 'react-native';
-// import React, {useState} from 'react';
-
-// const InputComponent = ({children}: any) => {
-//   const [value, setValue] = useState<string>('0');
-//   return (
-//     <>
-//       <TextInput
-//         keyboardType="phone-pad"
-//         value={value}
-//         placeholder="Temp value in Celsius"
-//         onChangeText={text => setValue(text)}
-//       />
-//       {children(value)}
-//     </>
-//   );
-// };
-
-// const Kelvin = ({value}: {value: string}) => {
-//   return (
-//     <View className="bg-orange-500 px-4 py-3">
-//       <Text className="text-black font-spacegrotesk-medium text-lg">
-//         Kelvin: {(parseFloat(value) + 273.15).toFixed(2)}
-//       </Text>
-//     </View>
-//   );
-// };
-
-// const Fahrenheit = ({value}: {value: string}) => {
-//   return (
-//     <View className="bg-orange-500 px-4 py-3">
-//       <Text className="text-black font-spacegrotesk-medium text-lg">
-//         Fahrenheit: {((parseFloat(value) * 9) / 5 + 32).toFixed(2)}
-//       </Text>
-//     </View>
-//   );
-// };
-
-// const HocTestScreen = () => {
-//   return (
-//     <View>
-//       <Text>HocTestScreen</Text>
-//       <InputComponent>
-//         {(value: string) => (
-//           <>
-//             <Fahrenheit value={value} />
-//             <Kelvin value={value} />
-//           </>
-//         )}
-//       </InputComponent>
-//     </View>
-//   );
-// };
-
-// export default HocTestScreen;
